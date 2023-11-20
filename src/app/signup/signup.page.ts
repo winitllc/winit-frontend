edbcf0c1d95d60e2f052e5bac08f14613854da85
haseebbutt687 <a.haseeb.butt8515@gmail.com>
2023-11-20 18:44:13 +0500

    WIP - signup as health pro. getting an error in the API response.

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { AlertController, LoadingController, NavController } from '@ionic/angular';
+import { AuthService } from '../util/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -9,16 +10,23 @@ import { NavController } from '@ionic/angular';
 export class SignupPage implements OnInit {
 
   userData = {
-    "fName": "",
-    "lName": "",
+    "firstName": "",
+    "lastName": "",
     "email": "",
     "confirmEmail": "",
-    "password": ""
+    "newPassword": "",
+    "phoneNumber": "",
+    "dateOfBirth": "",
+    "timezone": ""
   };
+  loading: any;
 
 
   constructor(
     private navCtrl: NavController,
+    private authService: AuthService,
+    private alertController: AlertController,
+    private loadingController: LoadingController
   ) { }
 
   ngOnInit() {
@@ -33,6 +41,65 @@ export class SignupPage implements OnInit {
   registerAction() {
     console.log('this is register action');
     console.log(this.userData);
+
+    if (this.userData.firstName == '' || this.userData.lastName == '' || this.userData.email == '' || this.userData.confirmEmail == '' || this.userData.newPassword == '' || this.userData.phoneNumber == '' || this.userData.dateOfBirth == '') {
+      this.presentAlert();
+    } else {
+      if (this.userData.email != this.userData.confirmEmail) {
+        this.presentAlertMultipleButtons('Email & Confirm Email Do Not Match');
+      }
+      else {
+        this.presentLoading();
+        this.authService.registerAPI(this.userData).subscribe(async (response) => {
+          console.log(response);
+          if (response['statusCode'] != 200) {
+            this.loading.dismiss();
+            this.presentAlertMultipleButtons(response['data']);
+          } else if (response['statusCode'] == 200) {
+            this.loading.dismiss();
+            const userData = response['data'];
+            console.log(userData);
+            this.navCtrl.navigateRoot('/signin');
+          }
+        },
+          (error) => {
+            console.log('this is error');
+            this.loading.dismiss();
+            this.presentAlertMultipleButtons(error['error']['data'][0]['message']);
+            console.log(error);
+          });
+      }
+    }
+  }
+
+  async presentAlertMultipleButtons(msg: string) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: msg,
+
+      buttons: ['Okay'],
+    });
+    await alert.present();
+  }
+
+  async presentLoading() {
+    this.loading = await this.loadingController.create({
+      message: 'Please Wait',
+    });
+    this.loading.backdropDismiss = false;
+    await this.loading.present();
+
+    const { role, data } = await this.loading.onDidDismiss();
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Alert',
+      message: 'Please Enter All Fields To Continue',
+      buttons: ['Okay'],
+    });
+    await alert.present();
   }
 
 }
