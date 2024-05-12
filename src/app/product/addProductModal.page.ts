b95cfd6a76ac23c6f79f160fd20a4571b6a48852
robert glenn <robertglenn@gmail.com>
2024-05-12 00:56:51 -0700

    getting somewhere; can post the jpeg base64 to lambda to decode into a bytepath and upload to s3 (then can use rekognition to detect text in the uploaded image)

--- src/app/product/addProductModal.page.ts
+++ src/app/product/addProductModal.page.ts
@@ -1,6 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
-
 import { ModalController } from '@ionic/angular';
 import ImageService from '../util/image.service';
 
@@ -44,65 +42,21 @@ export class AddProductModalPage implements OnInit {
     console.log(`AddProductModalComponent.imageToText: image to text selected`);
     try {
       console.log(`AddProductModalComponent.imageToText: running imageToText function`);
-      // const imageData = await this.captureImagePhoto();
-      const imageData = await this.captureImageBase64();
-      console.log(`AddProductModalComponent.imageToText: imageData data: ${JSON.stringify(imageData)}.`);
-      // console.log(`AddProductModalComponent.imageToText: length of imageData data: ${imageData.webPath?.length}.`);
-      // console.log(`AddProductModalComponent.imageToText: reversed imageData data: ${imageData.webPath?.split('').reverse().join('')}.`);
-      // const imageText = await this.imageService.imageToText(imageData.path || '');
-      const imageText = await this.imageService.imageToText(imageData || '');
+      const imageData = await this.imageService.captureImageDataURL();
+      console.log(`AddProductModalComponent.imageToText: imageData data: ${JSON.stringify(imageData)}`);
+      const rawImageData = imageData.replace('data:image/jpeg;base64,', '');
+      console.log(`AddProductModalComponent.imageToText: rawImageData data: ${JSON.stringify(rawImageData)}`);
+      const imageKeyInS3 = await this.imageService.callUploadToS3(rawImageData);
+      const imageText = await this.imageService.imageToText(imageKeyInS3);
       console.log(`AddProductModalComponent.imageToText: text from service: ${imageText}.`);
-      // this.imageSrc = imageData.webPath || '';
-      this.imageSrc = imageData || '';
+      // this.imageSrc = imageData;
       return {
-        // text: 'TBD',
         text: imageText,
-        // image: imageData.webPath || ''
         image: imageData || ''
       };
     } catch (error) {
       console.error(`AddProductModalComponent.imageToText: error capturing image and converting to text: ${JSON.stringify(error)}`);
       return this.imageToText();
-    }
-  }
-
-  async captureImagePhoto(): Promise<Photo> {
-    console.log(`AddProductModalPage.captureImagePhoto: capture image selected`);
-    try {
-      const imageData = await Camera.getPhoto({
-        quality: 2,
-        allowEditing: true,
-        resultType: CameraResultType.Uri,
-        source: CameraSource.Camera
-      });
-      console.log(`AddProductModalComponent.captureImagePhoto: imageData: ${JSON.stringify(imageData)}`);
-      return imageData || {
-        "webPath":"",
-        "exif":{},
-        "format":"",
-        "saved":false,
-        "path":""
-      };
-    } catch (error) {
-      console.error(`AddProductModalPage.captureImagePhoto: error from camera: ${JSON.stringify(error)}`);
-      throw error;
-    }
-  }
-
-  async captureImageBase64(): Promise<string> {
-    console.log(`AddProductModalPage.captureImageBase64: capture image selected`);
-    try {
-      const imageData = await Camera.getPhoto({
-        quality: 2,
-        allowEditing: true,
-        resultType: CameraResultType.Base64,
-        source: CameraSource.Camera
-      });
-      console.log(`AddProductModalComponent.captureImageBase64: imageData: ${JSON.stringify(imageData)}`);
-      return imageData.base64String || '';
-    } catch (error) {
-      console.error(`AddProductModalPage.captureImageBase64: error from camera: ${JSON.stringify(error)}`);
-      throw error;
     }
   }
 }
--- src/app/util/image.service.ts
+++ src/app/util/image.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { EnvironmentConfig } from '../environment.config';
 import { DetectTextCommand, DetectTextCommandInput, RekognitionClient } from "@aws-sdk/client-rekognition";
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 import { AuthState } from './auth.state';
 import { util } from 'wuzinit-common';
-import { Directory, Encoding, Filesystem } from '@capacitor/filesystem';
+import { CapacitorHttp, HttpOptions } from '@capacitor/core';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ import { Directory, Encoding, Filesystem } from '@capacitor/filesystem';
 export default class ImageService {
 
   credentials: util.AWSCredentials;
-  // rekognitionClient;
+  rekognitionClient;
 
   constructor(
     private auth: AuthState
@@ -19,54 +20,65 @@ export default class ImageService {
     const credentials: util.AWSCredentials = this.auth.getIAMCredentials();
     this.credentials = credentials;
     console.log(`ImageService.constructor: IAM credentials: ${JSON.stringify(credentials)}`);
-    // this.rekognitionClient = new RekognitionClient({
-    //   region: 'us-west-2',
-    //   credentials: {
-    //     accessKeyId: this.credentials.accessKeyId,
-    //     secretAccessKey: this.credentials.secretAccessKey
-    //   },
-    // });
+    this.rekognitionClient = new RekognitionClient({
+      region: 'us-west-2',
+      credentials: {
+        accessKeyId: this.credentials.accessKeyId,
+        secretAccessKey: this.credentials.secretAccessKey
+      },
+    });
   }
 
-  public async imageToText(imageToConvert: string) {
-    console.log(`ImageService.imageToText: image to convert to text: ${imageToConvert}`);
+  async callUploadToS3(imageToUpload: string): Promise<string> {
+    console.log(`ImageService.callUploadToS3: image to upload ${imageToUpload}`);
+    const hashName = `testName`;
+    const filename = `${Date.now()}-${hashName}.jpg`;
+    const imageKey = `imageToText/${filename}`;
+    console.log(`ImageService.uploadImageToS3: imageKey: ${imageKey}`);
+    const callUploadToS3URL = EnvironmentConfig.api.imageService.baseUrl + EnvironmentConfig.api.imageService.uploadToS3;
+    const requestOptions: HttpOptions = {
+      url: callUploadToS3URL,
+      headers: {
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/json'
+      },
+      data: {
+        imageKey,
+        imageToUpload
+      }
+    };
+    console.log(`ImageService.uploadImageToS3: uploadBody prepared: ${JSON.stringify(requestOptions)}`);
     try {
-      const rekognitionClient = new RekognitionClient({
-        region: 'us-west-2',
-        credentials: {
-          accessKeyId: this.credentials.accessKeyId,
-          secretAccessKey: this.credentials.secretAccessKey
-        },
-      });
+      const uploadToS3Response: any = await CapacitorHttp.post(requestOptions);
+      if (uploadToS3Response.status == 200) {
+        console.log(`ImageService.callUploadToS3:  ${JSON.stringify(uploadToS3Response)}`);
+        return imageKey;
+      } else {
+        throw new Error(`ImageService.callUploadToS3 - call to backend: ${JSON.stringify(uploadToS3Response)}`);
+      }
+    } catch (error) {
+      console.error(`ImageService.callUploadToS3: error ${error}`);
+      throw error;
+    }
+  }
+
+  public async imageToText(imageKeyInS3: string) {
+    console.log(`ImageService.imageToText: image to convert to text: ${imageKeyInS3}`);
+    try {
       console.log(`ImageService.imageToText: rekognition client set up`);
-      // const bytes = new Uint8Array(imageToConvert.length);
-      // bytes.forEach((byte, idx) => {
-      //   bytes.set([imageToConvert.charCodeAt(idx)], idx);
-      // });
-      // console.log(`ImageService.imageToText: image bytes length: ${JSON.stringify(bytes.length)}`);
-      const bytes = new TextEncoder().encode(imageToConvert);
       const detectTextOptions: DetectTextCommandInput = {
         Image: {
-          // Bytes: bytes
           S3Object: {
             Bucket: 'wuzinit-product-images-bucket',
-            Name: 'wuzinit-text.jpg'
+            Name: imageKeyInS3
           }
         }
       };
-      // const contents = await Filesystem.readFile({
-      //   path: imageToConvert
-      // });
-      // console.log(`ImageService.imageToText: image bytes length: ${JSON.stringify(contents)}`);
-      // const detectTextOptions: DetectTextCommandInput = {
-      //   "Image": {
-      //     "Bytes": new TextEncoder().encode(contents.data as string)
-      //     // "Bytes": bytes
-      //   }
-      // };
       console.log(`ImageService.imageToText: detect text options: ${JSON.stringify(detectTextOptions)}`);
       const rekognitionCommand = new DetectTextCommand(detectTextOptions);
-      const rekognitionResponse = await rekognitionClient.send(rekognitionCommand);
+      const rekognitionResponse = await this.rekognitionClient.send(rekognitionCommand);
       console.log(`ImageService.imageToText: response from rekognition: ${JSON.stringify(rekognitionResponse)}`);
       return 'TBD';
     } catch (error) {
@@ -75,21 +87,62 @@ export default class ImageService {
     }
   }
 
-  // public async saveImage(imageData: string, imageKey: string): Promise<void> {
-  //  if (imageData.includes(';base64,')) {
-  //    imageData = imageData.split(';base64,')[1];
-  //  }
-  //  const newImageParams = {
-  //    Body: new Buffer(imageData, 'base64'),
-  //    Bucket: EnvironmentConfig.s3.bucket,
-  //    Key: imageKey
-  //  };
-  //   try {
-  //     const putObjectResponse = await this.s3.putObject(newImageParams).promise();
-  //     console.log(`ImageService.saveImage: response from s3 putObject: ${JSON.stringify(putObjectResponse)}`);
-  //   } catch (error) {
-  //     console.error(`ImageService.saveImage: error uploading the image to s3: ${JSON.stringify(error)}`);
-  //     throw error;
-  //   }
-  // }
+  async captureImagePhoto(): Promise<Photo> {
+    console.log(`AddProductModalPage.captureImagePhoto: capture image selected`);
+    try {
+      const imageData = await Camera.getPhoto({
+        quality: 2,
+        allowEditing: true,
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Camera
+      });
+      console.log(`AddProductModalComponent.captureImagePhoto: imageData: ${JSON.stringify(imageData)}`);
+      return imageData || {
+        "webPath":"",
+        "exif":{},
+        "format":"",
+        "saved":false,
+        "path":""
+      };
+    } catch (error) {
+      console.error(`AddProductModalPage.captureImagePhoto: error from camera: ${JSON.stringify(error)}`);
+      throw error;
+    }
+  }
+
+  async captureImageDataURL(): Promise<string> {
+    console.log(`AddProductModalPage.captureImageBase64: capture image selected`);
+    try {
+      const imageData = await Camera.getPhoto({
+        quality: 2,
+        allowEditing: true,
+        resultType: CameraResultType.DataUrl,
+        source: CameraSource.Camera
+      });
+      console.log(`AddProductModalComponent.captureImageBase64: imageData keys: ${JSON.stringify(Object.keys(imageData))}`);
+      // console.log(`AddProductModalComponent.captureImageBase64: imageData: ${JSON.stringify(imageData)}`);
+      return imageData.dataUrl || '';
+    } catch (error) {
+      console.error(`AddProductModalPage.captureImageBase64: error from camera: ${JSON.stringify(error)}`);
+      throw error;
+    }
+  }
+
+  async captureImageBase64(): Promise<string> {
+    console.log(`AddProductModalPage.captureImageBase64: capture image selected`);
+    try {
+      const imageData = await Camera.getPhoto({
+        quality: 2,
+        allowEditing: true,
+        resultType: CameraResultType.Base64,
+        source: CameraSource.Camera
+      });
+      console.log(`AddProductModalComponent.captureImageBase64: imageData keys: ${JSON.stringify(Object.keys(imageData))}`);
+      // console.log(`AddProductModalComponent.captureImageBase64: imageData: ${JSON.stringify(imageData)}`);
+      return imageData.base64String || '';
+    } catch (error) {
+      console.error(`AddProductModalPage.captureImageBase64: error from camera: ${JSON.stringify(error)}`);
+      throw error;
+    }
+  }
 }
